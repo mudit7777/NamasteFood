15781aad909b2afab5359b22d8d452a6a9cce26f
ARORA Mudit <mudit.arora@soprasteria.com>
2024-10-22 15:27:42 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading = React.createElement("h1", { id: "heading" }, "Mudit💥");
+// const Title = () => (
+//   <div>
+//     <h1 className="titleComponent">this is Title Component</h1>
+//   </div>
+// );
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
