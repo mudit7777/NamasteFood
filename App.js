4f39c0519a789f500d57069b29a9a6ba5bb33ce4
ARORA Mudit <mudit.arora@soprasteria.com>
2024-10-22 19:37:35 +0530

    Built UI Components

--- App.js
+++ App.js
@@ -1,10 +1,90 @@
-const heading = React.createElement("h1", { id: "heading" }, "Mudit💥");
-// const Title = () => (
-//   <div>
-//     <h1 className="titleComponent">this is Title Component</h1>
-//   </div>
-// );
-console.log(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/**
+ * Header
+ * - Logo
+ * - Navbar
+ * Body
+ * - Search
+ * - Restaurant Cards
+ *   - Images
+ *   - Name of Res, Rating, Cuisine, etc
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+ *
+ */
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwv45iAbWWwD5glterGTBIO6Q6pnjukcFkGw&s"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search"> Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1KOEdWcpGrFkUZzsSt1QwEUBiuLKvDydEsLh2fQ-A6YA5dr1H_0k-Fo9_8bDuWb22RBY&usqp=CAU"
+      ></img>
+      <h3>Mudit Foods</h3>
+      <h4>North Indian, Italian</h4>
+      <h4>4.8⭐</h4>
+
+      <h4>35 minutes</h4>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<AppLayout />);
