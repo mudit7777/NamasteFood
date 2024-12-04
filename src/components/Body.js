402a984229083ce76740d227158c2a65fcc5b4ae
ARORA Mudit <mudit.arora@soprasteria.com>
2024-12-04 14:43:25 +0530

    Applied Search Functionality and Shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,71 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import { useEffect, useReducer, useState } from "react";
 
 const Body = () => {
   // State Variable / Local State Variable  - Super Powerful Variable
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredRestraunt, setFilteredRestraunts] = useState([]);
 
-  return (
+  // Whenever state variable update, react triggers a reconcilation cycle(re-renders the component)
+
+  console.log("Body Rendered ");
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // react's react fibre, the reconcilation process, that updates DOM when required
+  // Also update only that part of the DOM which is required to update
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.61450&lng=77.30630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+    // Optional Chaining
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestraunts(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              console.log(searchText);
+              const filteredRestraunt = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestraunts(filteredRestraunt);
+            }}
+
+            // Filter the restaurant cards and update the UI
+            // Search text
+            // take the value of the input box
+            // bind the value to the local state variable of react
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
@@ -16,7 +73,7 @@ const Body = () => {
             const filteredResList = listOfRestaurants.filter(
               (res) => res.info.avgRating > 4
             );
-            setListOfRestaurants(filteredResList);
+            setFilteredRestraunts(filteredResList);
             console.log(filteredResList);
           }}
         >
@@ -27,7 +84,7 @@ const Body = () => {
         {
           // loop over all restaurants
           // for each restraunt we have to return a restaurant
-          listOfRestaurants.map((restaurant) => (
+          filteredRestraunt.map((restaurant) => (
             <RestaurantCard key={restaurant.info.id} resData={restaurant} />
           ))
         }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,22 @@
 import { LOGO_URL } from "../utils/constants";
+import { useEffect, useState } from "react";
+
 const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+  console.log("Body Header Rendered");
+
+  useEffect(() => {
+    console.log("UseEffect Called ");
+  }, []);
+
+  // UseEffect will be called every time the component renderes
+  // So if we put dependency array in UseEffect it changes the behaviour of UseEffect
+  // If no dependency array, then it's called on every render
+  // If Dependency Array, then called on bases of dependency Array, it'll only be called when the dependency changes
+  // If the dependency array is {btnNameReact} then it will be called whenever btnNameReact changes
+
+  // If the dependency array is an empty = [] ==> then useEffect is called only on initial render(just once )
+  // and just Once
   return (
     <div className="header">
       <div className="logo-container">
@@ -11,6 +28,17 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+              console.log(btnNameReact);
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
