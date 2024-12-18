1af6fe3ad3adc9cc85e0d2e5111b7c2a287101e2
ARORA Mudit <mudit.arora@soprasteria.com>
2024-12-18 17:52:30 +0530

    Deep Story - Class Component and LifeCycle Methods

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,31 @@
 import UserClass from "./UserClass";
-const About = () => {
-  return (
-    <div>
-      <h1>This is About Component</h1>
+import { Component } from "react";
 
-      <UserClass
-        name={"Mudit (Class)"}
-        location={"Thari range te bahar"}
-        contact={"9050055553"}
-      />
-    </div>
-  );
-};
+class About extends Component {
+  // class constructor
+  constructor(props) {
+    super(props);
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent Component Did Mount");
+  }
+
+  render() {
+    console.log("Parent Render");
+    return (
+      <div>
+        <h1>This is About Component</h1>
+
+        <UserClass
+          name={"First (Class)"}
+          location={"FirstPur"}
+          contact={"7777788888"}
+        />
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,50 +1,72 @@
 import React from "react";
-class UserClass extends React.Component {
+import { Component } from "react";
+class UserClass extends Component {
   // how do we pass props in the class based component
   constructor(props) {
     super(props);
-    console.log(props);
+    console.log(this.props.name + "Child Constructor");
 
     // defining state
     this.state = {
-      count: 1,
-      count2: 10,
+      userInfo: {
+        name: "Dummy",
+        blog: "",
+        avatar_url: "Default.com",
+      },
     };
   }
+
+  async componentDidMount() {
+    console.log("Child Component Did Mount ");
+    // API Call
+    const data = await fetch("https://api.github.com/users/mudit7777");
+    const json = await data.json();
+
+    // set State
+    this.setState({
+      userInfo: json,
+    });
+
+    console.log(json);
+  }
+
+  componentDidUpdate() {
+    console.log("Component Did Update");
+  }
+
+  componentWillUnmount() {
+    console.log("Component Will Unmount");
+  }
+
   render() {
-    // initialise the props
-    const { name, location, contact } = this.props;
-    const { count, count2 } = this.state;
+    const { name, blog, avatar_url, login } = this.state.userInfo;
     return (
       <div className="user-card">
-        <h1>Count : {count}</h1>
-        <button
-          onClick={() => {
-            // NEVER UPDATE STATE VARIBALE DIRECTLY
-            this.setState({
-              count: this.state.count + 1,
-            });
-          }}
-        >
-          Count Increase{" "}
-        </button>
-        <h1>Count : {count2}</h1>
-        <button
-          onClick={() => {
-            // NEVER UPDATE STATE VARIBALE DIRECTLY
-            this.setState({
-              count2: this.state.count2 + 1,
-            });
-          }}
-        >
-          Count Increase Second{" "}
-        </button>
+        <img src={avatar_url}></img>
         <h2>Name : {name}</h2>
-        <h2>Location : {location}</h2>
-        <h2>Contact : {contact}</h2>
+        <h2>Blog : {blog}</h2>
+        <h2>Login : {login} </h2>
       </div>
     );
   }
 }
 
 export default UserClass;
+
+/**
+ * ---Mounting---
+ * Constructor (dummy)
+ * Render (dummy)
+ *    <HTML Dummy>
+ * Component Did Mount
+ *    <API Call>
+ *    <this.setState> -> State Variable Update
+ * Once the this.setState is called, it calls the render() method again
+ *
+ * --UPDATE
+ *
+ * render(API Data)
+ * <HTML (new API Data)
+ * componentDidUpdate
+ *
+ */
